2b6887ba0e9b6e7b67f52b88b20ffcc05181823a
Web Dev Koushal <95288059+Koushlendra-Kumar@users.noreply.github.com>
2023-08-29 03:56:40 +0000

    Sync LeetCode submission - To Be Or Not To Be (javascript)

---
+++ Leetcode/problems/to_be_or_not_to_be/solution.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(anotherVal){
+            if(val === anotherVal) return true;
+            else {
+                throw "Not Equal";
+            }
+        },
+        notToBe(anotherVal){
+            if(anotherVal !== val) return true;
+            else {
+                throw "Equal";
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
